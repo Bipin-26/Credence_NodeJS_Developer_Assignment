a041801d9fe03033b158b5727ff54c4dbddda61c
Bipin Memana <bipinmw98@gmail.com>
2024-07-25 20:32:30 +0530

    updated controller.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    // const conn = await mongoose.connect(process.env.LOCALHOST_MONGO_URI);
+
+    // if the below given connection string doesn't work then make sure to use above localhost uri
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connection successful: ${conn.connection.host}`);
   } catch (err) {
--- controller/movieController.js
+++ controller/movieController.js
@@ -22,30 +22,30 @@ const viewMovie = async (req, res) => {
 
 // controller function to add new movie to db
 const addMovie = async (req, res) => {
-  const { name, img, summary } = req.body;
-  const movieExists = await Movie.findOne({ name: name });
-  if (movieExists) {
-    logger.info("Movie already exists");
-    res.status(400);
-    res.send("Movie already exists");
-  } else {
-    try {
+  try {
+    const { name, img, summary } = req.body;
+    const movieExists = await Movie.findOne({ name: name });
+    if (movieExists) {
+      logger.info("Movie already exists");
+      res.status(400);
+      res.send("Movie already exists");
+    } else {
       const movie = await Movie.create({ name, img, summary });
       logger.info({ message: "Movie added successfully", movie });
       res.status(201);
       res.send({ message: "Movie added successfully", movie });
-    } catch (err) {
-      logger.error(err);
-      res.status(400).send("Something went wrong");
     }
+  } catch (err) {
+    logger.error(err);
+    res.status(400).send("Something went wrong");
   }
 };
 
 // controller function to update a movie based on the id passed as param, and updated body values.
 const updateMovie = async (req, res) => {
-  const id = req.query.id;
-  const { name, img, summary } = req.body;
   try {
+    const id = req.query.id;
+    const { name, img, summary } = req.body;
     const movie = await Movie.findById(id);
     if (movie) {
       movie.name = name || movie.name;
@@ -72,8 +72,8 @@ const updateMovie = async (req, res) => {
 
 // controller function to delete a movie based on the id passed as param
 const deleteMovie = async (req, res) => {
-  const id = req.query.id;
   try {
+    const id = req.query.id;
     const movieExists = await Movie.findById(id);
     if (movieExists) {
       await Movie.deleteOne({ _id: id });
